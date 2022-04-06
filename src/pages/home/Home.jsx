7fa480bf6b4aaf26242b9bf440be4bcd0fd86b7f
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-04-06 14:49:16 +0630

    navbar added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Sidebar from '../../components/sidebar';
+import Navbar from "../../components/navbar";
 import  "./home.scss";
 const Home = () => {
   return (
     <div className='home'>
       <Sidebar/>
-      <div className="homeContainer">container</div>
+      <div className="homeContainer">
+        <Navbar/>
+        Home Contents
+      </div>
     </div>
   )
 }
