71125835aea9e7807f0558a32ea147acd1a4f6ed
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-04-07 10:57:18 +0630

    dashboard home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Sidebar from '../../components/sidebar';
 import Navbar from "../../components/navbar";
 import  "./home.scss";
+import Widget from '../../components/widget';
 const Home = () => {
   return (
     <div className='home'>
       <Sidebar/>
       <div className="homeContainer">
         <Navbar/>
-        Home Contents
+        <div className="widgets">
+          <Widget type="Customers"/>
+          <Widget type="Orders"/>
+          <Widget type="Total Books"/>
+          <Widget type="Total Sales"/>
+        </div>
       </div>
     </div>
   )
